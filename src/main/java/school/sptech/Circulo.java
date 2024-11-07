9663f3f5094901ce83d73d73d97e788372b0400f
de Oliveira <denyel.h.de.oliveira@accenture.com>
2024-11-07 18:23:42 -0300

    Subindo tudo ok

--- src/main/java/school/sptech/Circulo.java
+++ src/main/java/school/sptech/Circulo.java
@@ -1,4 +1,25 @@
 package school.sptech;
 
-public class Circulo {
+public class Circulo extends Figura{
+    private Double raio;
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
+
+    @Override public Double calcularArea(){
+        Double retorno = (getRaio() * getRaio()) * Math.PI;
+        return retorno;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo{" +
+                "raio=" + raio +
+                '}';
+    }
 }
